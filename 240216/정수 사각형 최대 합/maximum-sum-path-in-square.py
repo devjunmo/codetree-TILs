63bf23900113728f38c5d2c05d82642b16970c11
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-02-16 22:39:18 +0900

    Title: 정수 사각형 최대 합, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240216/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().strip().split(' '))))
+
+dp = []
+for _  in range(n):
+    dp.append([0]*n)
+
+dp[0][0] = arr[0][0]
+
+# 초기값 채우기
+for i in range(1, n):
+    dp[0][i] = dp[0][i-1] + arr[0][i]
+
+for i in range(1, n):
+    dp[i][0] = dp[i-1][0] + arr[i][0]
+
+# dp 채우기
+# 위쪽 왼쪽 
+res = -1
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(dp[i-1][j]+arr[i][j], dp[i][j-1]+arr[i][j])
+        res = max(res, dp[i][j])
+
+print(res)
