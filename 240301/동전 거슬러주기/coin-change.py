4acfd4ca29bf6e8e89ed95ef3e9339c2b33995e8
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-03-01 18:24:23 +0900

    Title: 동전 거슬러주기, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240301/동전 거슬러주기/coin-change.py
+++ 240301/동전 거슬러주기/coin-change.py
@@ -1,16 +1,17 @@
-n, m = tuple(map(int, input().strip().split(" ")))
-arr = list(map(int, input().strip().split(" ")))
+n, m = tuple(map(int, input().strip().split(" "))) # m = 11
+arr = list(map(int, input().strip().split(" "))) # 3 5 7
 
-dp = [0] * (m+1)
-dp[1] = 1
+dp = [0] * (m+1) 
 
-for i in range(2, m+1):
+# 금액 i를 거슬러 주기 위한 
+for i in range(1, m+1):
+    # print(i)
     cur_max_dp_cnt = -1
     for j in arr:
-        gap = j-i
-        if gap >= 0:
+        gap = i-j
+        if gap >= 0 and (dp[gap] != 0 or gap == 0):
             cur_max_dp_cnt = max(cur_max_dp_cnt, dp[gap]+1)
             dp[i] = cur_max_dp_cnt
-    # print(dp)=
+    # print(dp)
 
 print(max(dp))
