2ffccc78667671ffdc09e571cfdd598bf6977dfc
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-03-28 21:07:45 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240328/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,35 @@
+"""
+4 3 1
+0 0 0 0
+0 0 0 1
+1 0 0 1
+1 1 1 1
+"""
+
+n, m, k = tuple(map(int, input().strip().split(' ')))
+arr = [
+    list(map(int, input().strip().split(' ')))
+    for _ in range(n)
+]
+start_col = k-1
+end_col = k-1 + m-1
+start_row = 0
+is_finish = False
+
+while True:
+    # 블럭조각 순회하며 밑에 있는 모든 블럭이 0인지 체크
+    for j in range(start_col, end_col+1):
+        if arr[start_row+1][j] == 1:
+            is_finish = True
+            break
+    if is_finish:
+        break
+    
+    # 모두 영이라면 블럭 전진
+    start_row += 1
+    for j in range(start_col, end_col+1):
+        arr[start_row][j] = 1
+        arr[start_row-1][j] = 0
+
+for row in arr:
+    print(*row)
