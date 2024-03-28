e92d5acefcd12b4e04664f7738129a89a7a3923b
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-03-28 21:09:19 +0900

    Title: 떨어지는 1자 블록, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240328/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240328/떨어지는 1자 블록/falling-horizontal-block.py
@@ -16,20 +16,24 @@ end_col = k-1 + m-1
 start_row = 0
 is_finish = False
 
-while True:
-    # 블럭조각 순회하며 밑에 있는 모든 블럭이 0인지 체크
-    for j in range(start_col, end_col+1):
-        if arr[start_row+1][j] == 1:
-            is_finish = True
+if n == 1:
+    for row in arr:
+        print(*row)
+else:
+    while True:
+        # 블럭조각 순회하며 밑에 있는 모든 블럭이 0인지 체크
+        for j in range(start_col, end_col+1):
+            if arr[start_row+1][j] == 1:
+                is_finish = True
+                break
+        if is_finish:
             break
-    if is_finish:
-        break
-    
-    # 모두 영이라면 블럭 전진
-    start_row += 1
-    for j in range(start_col, end_col+1):
-        arr[start_row][j] = 1
-        arr[start_row-1][j] = 0
+        
+        # 모두 영이라면 블럭 전진
+        start_row += 1
+        for j in range(start_col, end_col+1):
+            arr[start_row][j] = 1
+            arr[start_row-1][j] = 0
 
-for row in arr:
-    print(*row)
+    for row in arr:
+        print(*row)
