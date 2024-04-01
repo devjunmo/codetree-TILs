864a723b0a48ca82d5b9d903864802be71e45a05
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-04-01 21:30:14 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240401/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
+++ 240401/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
@@ -31,22 +31,24 @@ max_cnt = 0
 
 def simulate(cnt, r, c):
     global max_cnt
-
     d = dir_arr[r][c]
-    nx = r + dx[d]
-    ny = c + dy[d]
-    # 정상범위 일 때
-    if 0 <= nx < n and 0 <= ny < n:
-        next_val = num_arr[nx][ny]
-        cur_val = num_arr[r][c]
-        # 만약 다음 방향의 값이 현재 값보다 같거나 작다면
-        if next_val <= cur_val:
-            # max랑 비교 후 return
-            max_cnt = max(max_cnt, cnt)
-            return
+    for i in range(1, 10):
+        nx = r + i * dx[d]
+        ny = c + i * dy[d]
+        # 정상범위 일 때
+        if 0 <= nx < n and 0 <= ny < n:
+            next_val = num_arr[nx][ny]
+            cur_val = num_arr[r][c]
+            # 만약 다음 방향의 값이 현재 값보다 같거나 작다면
+            if next_val <= cur_val:
+                # max랑 비교 후 return
+                max_cnt = max(max_cnt, cnt)
+                return
+            else:
+                simulate(cnt+1, nx, ny)
         else:
-            simulate(cnt+1, nx, ny)
+            break
 
-simulate(1, r, c)
+simulate(0, r, c)
 
 print(max_cnt)
