e99e3300dc6fcf30e08933d275843f347640f96b
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-04-01 21:59:01 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240401/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
+++ 240401/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
@@ -1,14 +1,3 @@
-"""
-3
-7 1 4
-2 6 3
-9 8 5
-5 3 1
-6 3 7
-2 4 8
-3 3
-"""
-
 n = int(input())
 num_arr = [
     list(map(int, input().strip().split(' ')))
@@ -16,6 +5,10 @@ num_arr = [
 ]
 dir_arr = [
     list(map(int, input().strip().split(' ')))
+    for _ in range(n)
+]
+vis = [
+    [False] * n
     for _ in range(n)
 ]
 
@@ -32,23 +25,32 @@ max_cnt = 0
 def simulate(cnt, r, c):
     global max_cnt
     d = dir_arr[r][c]
-    for i in range(1, 10):
-        nx = r + i * dx[d]
-        ny = c + i * dy[d]
-        # 정상범위 일 때
-        if 0 <= nx < n and 0 <= ny < n:
+    for i in range(1, 4):
+        nx = r + (i * dx[d])
+        ny = c + (i * dy[d])
+        # print(f'>>{nx}, {ny}')
+        # 정상범위면서 방문 안한곳일 때 
+        if 0 <= nx < n and 0 <= ny < n and not vis[nx][ny]:
             next_val = num_arr[nx][ny]
             cur_val = num_arr[r][c]
             # 만약 다음 방향의 값이 현재 값보다 같거나 작다면
             if next_val <= cur_val:
                 # max랑 비교 후 return
                 max_cnt = max(max_cnt, cnt)
+                vis[r][c] = False
                 return
             else:
+                vis[nx][ny] = True
                 simulate(cnt+1, nx, ny)
         else:
-            break
+            # print('움직 x')
+            max_cnt = max(max_cnt, cnt)
+            vis[r][c] = False
+            #return
+        if not (0 <= nx < n and 0 <= ny < n):
+            return    
 
+vis[r][c] = True
 simulate(0, r, c)
 
 print(max_cnt)
