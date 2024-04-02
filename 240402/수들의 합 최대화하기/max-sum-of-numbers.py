fbbd5808a7698f99cc7bdefb2c37a9e0b440e0a1
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-04-02 20:46:11 +0900

    Title: 수들의 합 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240402/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -0,0 +1,29 @@
+n=int(input())
+arr=[
+    list(map(int, input().split(' ')))
+    for _ in range(n)
+]
+max_val=0
+picks=[]
+vis=[False]*n
+
+def simul(lev):
+    global max_val
+    if lev==n:
+        max_val=max(max_val, sum(picks))
+        return
+    
+    for i in range(n):
+        if not vis[i]:
+            vis[i]=True
+            #print(lev)
+            #print(i)
+            #print()
+            picks.append(arr[lev][i])
+            simul(lev+1)
+            picks.pop()
+            vis[i]=False
+
+
+simul(0)
+print(max_val)
