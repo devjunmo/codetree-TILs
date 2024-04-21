7e4082443fdf15c18eb3c5ceb7ddd820d92a814a
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-04-21 18:28:05 +0900

    Title: 자리 바꾸기 2, Time: 685ms, Memory: 67MB, Status: Passed - CodeTree

--- 240421/자리 바꾸기 2/changing-seats-2.py
+++ 240421/자리 바꾸기 2/changing-seats-2.py
@@ -1,28 +1,28 @@
 n, k = tuple(map(int, input().split(' ')))
 
+moves = [
+    tuple(map(int, input().split(' ')))
+    for _ in range(k)
+]
+
+# 좌석 리스트 
+lst = [
+    i for i in range(n+1)
+]
+
 """
 d = {
     key: 1 ~ n
     val: set(방문한 인덱스)
 }
 """
-
-moves = [
-    tuple(map(int, input().split(' ')))
-    for _ in range(k)
-]
-
-lst = [
-    i for i in range(n+1)
-]
-
 d = dict()
 
 for i in range(1, n+1):
     d[i] = set([i])
 
 
-for _ in range(k):
+for _ in range(3):
     for mv in moves:
         idx1 = mv[0]
         idx2 = mv[1]
@@ -39,4 +39,6 @@ for _ in range(k):
         d[v2].add(idx1)
 
 for k, v in d.items():
+    # print(f'key: {k}')
     print(len(v))
+    # print(v)
