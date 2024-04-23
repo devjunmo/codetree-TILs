8a2a4697dfa90c97bbee3a0f8971caf77542faf6
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-04-23 21:41:52 +0900

    Title: 마지막으로 남은 숫자, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240423/마지막으로 남은 숫자/last-remaining-number.py
@@ -0,0 +1,18 @@
+import heapq
+
+n = int(input())
+pq = list(map(int, input().strip().split(' ')))
+pq = [-x for x in pq]
+
+while len(pq) > 1:
+    v1 = heapq.heappop(pq)
+    v2 = heapq.heappop(pq)
+    gap = abs(v1-v2)
+    heapq.heappush(pq, -gap)
+
+
+if len(pq) == 1:
+    print(-pq[0])
+
+if len(pq) == 0:
+    print(-1)
