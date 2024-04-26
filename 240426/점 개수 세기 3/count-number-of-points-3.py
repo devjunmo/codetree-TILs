f2b1528f63699c47419bab5ea9cfb43b4fab0848
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-04-26 22:56:02 +0900

    Title: 점 개수 세기 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240426/점 개수 세기 3/count-number-of-points-3.py
+++ 240426/점 개수 세기 3/count-number-of-points-3.py
@@ -14,7 +14,7 @@ while lst:
     cnt += 1
 
 for _ in range(q):
-    a, b = tuple(map(int, input().split(' ')))
+    a, b = tuple(map(int, input().strip().split(' ')))
     ca = d[a]
     cb = d[b]
     # print(f'ca: {ca}, cb: {cb}')
