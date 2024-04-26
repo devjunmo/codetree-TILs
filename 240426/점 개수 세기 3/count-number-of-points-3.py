b0e3481858676b00c87e1afc9b6e07396ac5169e
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-04-26 22:55:46 +0900

    Title: 점 개수 세기 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240426/점 개수 세기 3/count-number-of-points-3.py
@@ -0,0 +1,21 @@
+import heapq
+
+n, q = tuple(map(int, input().split(' ')))
+lst = list(map(int, input().split(' ')))
+
+heapq.heapify(lst)
+
+d = {}
+cnt = 1
+
+while lst:
+    v = heapq.heappop(lst)
+    d[v] = cnt
+    cnt += 1
+
+for _ in range(q):
+    a, b = tuple(map(int, input().split(' ')))
+    ca = d[a]
+    cb = d[b]
+    # print(f'ca: {ca}, cb: {cb}')
+    print(cb-ca+1)
