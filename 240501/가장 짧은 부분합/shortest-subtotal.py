3d31a7e9051ef33522230278ffe52d635f1f3d42
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-05-01 17:57:03 +0900

    Title: 가장 짧은 부분합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240501/가장 짧은 부분합/shortest-subtotal.py
@@ -0,0 +1,22 @@
+n, s = tuple(map(int, input().strip().split(' ')))
+
+arr = list(map(int, input().strip().split(' ')))
+# arr = [0] + arr
+
+
+min_len = 9999999
+sum_val = 0
+
+l_idx = 0
+
+for r_idx in range(n):
+    sum_val += arr[r_idx]
+
+    # 합이 s 이상일 때 길이 잰다
+    while sum_val >= s:
+        min_len = min(min_len, r_idx-l_idx+1)
+        # l_idx 전진
+        l_idx+=1
+        sum_val -= arr[l_idx]
+
+print(min_len)
