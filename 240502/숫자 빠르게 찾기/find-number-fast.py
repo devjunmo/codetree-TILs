9a2f1e91e1c5a96b6650799bde7672adea8d2707
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-05-02 22:18:03 +0900

    Title: 숫자 빠르게 찾기, Time: 416ms, Memory: 29MB, Status: Passed - CodeTree

--- 240502/숫자 빠르게 찾기/find-number-fast.py
+++ 240502/숫자 빠르게 찾기/find-number-fast.py
@@ -6,7 +6,7 @@ arr = list(map(int, input().strip().split(' ')))
 for i in range(m):
     cur_m = int(input())
     idx = bisect.bisect_left(arr, cur_m)
-    if arr[idx] == cur_m:
+    if idx < n and arr[idx] == cur_m:
         print(idx+1)
     else:
         print(-1)
