1516498493eebc853d28af0938eee14f7a64a629
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-05-05 19:10:26 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 124ms, Memory: 31MB, Status: Passed - CodeTree

--- 240505/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 240505/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -1,11 +1,16 @@
-n = int(input().strip())
-nums = list(map(int, input().strip().split()))
+def max_subarray_sum(nums):
+    max_ending_here = nums[0]
+    max_so_far = nums[0]
+    
+    for num in nums[1:]:
+        max_ending_here = max(max_ending_here + num, num)
+        max_so_far = max(max_so_far, max_ending_here)
+    
+    return max_so_far
 
-# max(cur_max + cur_val, cur_val)
+# 입력을 받는 코드
+n = int(input().strip())  # 원소의 개수 입력 받기
+nums = list(map(int, input().strip().split()))  # 공백으로 구분된 정수들을 입력 받아 리스트로 변환
 
-cur_max = 0
-
-for num in nums:
-    cur_max = max(cur_max + num, num)
-
-print(cur_max)
+# 최대 부분 배열 합 계산 및 출력
+print(max_subarray_sum(nums))
