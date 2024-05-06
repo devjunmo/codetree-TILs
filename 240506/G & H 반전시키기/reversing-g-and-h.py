aa2498eada6b9e40eaae44f9900d6ac7e7c02887
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-05-06 21:33:52 +0900

    Title: G & H 반전시키기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240506/G & H 반전시키기/reversing-g-and-h.py
@@ -0,0 +1,18 @@
+n = int(input().strip())
+initial = input().strip()
+target = input().strip()
+
+i = 0
+cnt = 0
+
+while i < n:
+    if initial[i] == target[i]:
+        i += 1
+        cnt += 1
+        # 매칭 되는 연속 구간 i 증가
+        while i < n and initial[i] == target[i]:
+            i+=1
+    else:
+        i += 1
+
+print(cnt)
