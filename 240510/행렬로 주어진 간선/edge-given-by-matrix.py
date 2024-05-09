aab01f620c934ed4ef53969482830ec34b22c188
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-05-10 00:01:56 +0900

    Title: 행렬로 주어진 간선, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240510/행렬로 주어진 간선/edge-given-by-matrix.py
@@ -0,0 +1,35 @@
+n = int(input())
+
+adj_lst = [
+    [0]+list(map(int, input().strip().split(' ')))
+    for _ in range(n)
+]
+adj_lst = [[0] * (n+1)] + adj_lst
+# print(*adj_lst, sep='\n')
+
+dist = [
+    [0] * (n+1)
+    for _ in range(n+1)
+]
+
+# 원래 갈수있는곳은 1
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if adj_lst[i][j] == 1:
+            dist[i][j] = 1
+
+# 제자리는 1
+for i in range(1, n+1):
+    dist[i][i] = 1
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if adj_lst[i][k] and adj_lst[k][j]:
+                dist[i][j] = 1
+
+for i in range(1, n+1):
+    buff = []
+    for j in range(1, n+1):
+        buff.append(dist[i][j])
+    print(*buff)
