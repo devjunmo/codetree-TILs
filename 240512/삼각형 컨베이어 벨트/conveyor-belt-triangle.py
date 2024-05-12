454cc6cc60aa067b7e48bce47bba9588285f758b
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-05-12 17:56:23 +0900

    Title: 삼각형 컨베이어 벨트, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240512/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
+++ 240512/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -2,28 +2,34 @@ n,t=tuple(map(int,input().strip().split(' ')))
 
 grid=[
     list(map(int,input().strip().split(' ')))
-    for _ in range(n)
+    for _ in range(3)
 ]
 
 tmp_arr = []
 
 def push_right(lst):
-    pass
+    tmp_arr.append(lst[n-1]) # 맨 끝값 백업 
+    # 한칸 밀기
+    for i in range(n-2, -1, -1):
+        lst[i+1] = lst[i]
 
 
 def set_tmp(t_idx):
-    pass
+    if t_idx == 2:
+        grid[0][0] = tmp_arr[t_idx]
+    else:
+        grid[t_idx+1][0] = tmp_arr[t_idx]
 
 
 def clean_tmp_arr():
-    pass
+    tmp_arr = []
 
 
 for sec in range(t):
     for row in grid:
         push_right(row)
     
-    for tmp_i in range(len(tmp_arr)):
+    for tmp_i in range(3):
         set_tmp(tmp_i)
 
 
