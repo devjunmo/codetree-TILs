e02fed61cb16566e235d5e22c7b4312d350cc364
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-05-13 18:52:37 +0900

    Title: 1차원 폭발 게임, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240513/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,54 @@
+n, m = tuple(map(int, input().strip().split(' ')))
+b_arr = [
+    int(input().strip())
+    for _ in range(n)
+]
+
+
+def find_boom():
+    # m번 이상 반복되는 구간을 리스트에 넣기
+    target_pos_lst = [] # [(a1, b1), (a2, b2), ...]
+    left = 0
+    right = 0
+    while right < len(b_arr):
+        lv = b_arr[left]
+        rv = b_arr[right]
+        if lv != rv:
+            gap = right - left
+            if gap >= m:
+                target_pos_lst.append((left, right-1))
+            left = right # 위치 동기화
+        right += 1
+    
+    # print(target_pos_lst)
+    return target_pos_lst
+
+
+def boom(target_pos_lst):
+    global b_arr
+    if not target_pos_lst:
+        return 0
+    # pos list의 범위를 모두 -1로 바꾸고
+    for pos in target_pos_lst:
+        a, b = pos
+        for i in range(a, b+1):
+            b_arr[i] = -1
+    
+    # -1이 아닌 값을 확보한 뒤 b_arr로 카피
+    after_arr = []
+    for b in b_arr:
+        if b != -1:
+            after_arr.append(b)
+    b_arr = after_arr
+    return 1
+
+
+while True:
+    res = boom(find_boom())
+    if res == 0:
+        break
+
+
+print(len(b_arr))
+for b in b_arr:
+    print(b)
