96bd3314464e1f73f115ab7c260f9bb401ab7abb
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-05-20 19:26:14 +0900

    Title: 외판원 순회, Time: 757ms, Memory: 66MB, Status: Passed - CodeTree

--- 240520/외판원 순회/traveling-salesman-problem.py
+++ 240520/외판원 순회/traveling-salesman-problem.py
@@ -10,14 +10,19 @@ adj_lst = [
 point_lst = [i for i in range(1,n)]
 
 vis_case = list(permutations(point_lst, n-1))
+#print(*vis_case,sep='\n')
 
 def calc(case_lst):
     case_lst = [0] + case_lst + [0]
+    #print(case_lst)
     sum_v = 0
     for i in range(len(case_lst)-1):
         p1 = case_lst[i]
         p2 = case_lst[i+1]
+        if adj_lst[p1][p2]==0:
+            return sys.maxsize
         sum_v += adj_lst[p1][p2]
+    #print(sum_v)
     return sum_v
     
 
