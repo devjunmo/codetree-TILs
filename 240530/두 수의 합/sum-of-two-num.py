96cae277e247a8f6199b1cc92522e2ab63fe9d5d
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-05-30 18:22:40 +0900

    Title: 두 수의 합, Time: 125ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240530/두 수의 합/sum-of-two-num.py
+++ 240530/두 수의 합/sum-of-two-num.py
@@ -10,7 +10,9 @@ for comp in arr:
 cnt=0
 for comp in arr:
     v=k-comp
-    if v in d:
+    if v in d and d[v] and comp in d and d[comp]:
         cnt+=1
+        d[comp]=False
+        d[v]=False
 
-print(int(cnt/2))
+print(int(cnt))
