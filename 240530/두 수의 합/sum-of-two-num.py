2878b2b0bfaec4007de7872d8bf9435fd1d620ed
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-05-30 19:15:34 +0900

    Title: 두 수의 합, Time: 1196ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240530/두 수의 합/sum-of-two-num.py
+++ 240530/두 수의 합/sum-of-two-num.py
@@ -1,18 +1,33 @@
+from itertools import combinations
+
 n,k=tuple(map(int,input().strip().split(' ')))
 arr=list(map(int,input().strip().split(' ')))
 
 d={}
-for comp in arr:
-    if comp in d:
-        continue
-    d[comp]=True
+vis=[False]*(n+10)
 
+for i in range(len(arr)):
+    if arr[i] in d:
+        d[arr[i]].append(i)
+        continue
+    d[arr[i]]=[i]
+#print(d)
 cnt=0
 for comp in arr:
+    if vis[d[comp][0]]:
+        continue
     v=k-comp
-    if v in d and d[v] and comp in d and d[comp]:
-        cnt+=1
-        d[comp]=False
-        d[v]=False
+    if comp==v:
+        cnt+=len(list(combinations(d[comp],2)))
+        for idx in d[comp]:
+            vis[idx]=True
+        continue
+    
+    if comp in d and v in d:
+        cnt+=(len(d[comp])*len(d[v]))
+        for idx in d[comp]:
+            vis[idx]=True
+        for idx in d[v]:
+            vis[idx]=True
 
 print(int(cnt))
