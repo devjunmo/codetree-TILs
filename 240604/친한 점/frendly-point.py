488d716c4e06c368cab998fda7a80718e94554e5
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-06-04 23:40:35 +0900

    Title: 친한 점, Time: 759ms, Memory: 39MB, Status: Wrong Answer - CodeTree

---
+++ 240604/친한 점/frendly-point.py
@@ -0,0 +1,32 @@
+from bisect import bisect_left, bisect_right
+
+n, m = tuple(map(int, input().strip().split(' ')))
+
+points = [
+    tuple(map(int, input().strip().split(' ')))
+    for _ in range(n)
+]
+
+targets = [
+    tuple(map(int, input().strip().split(' ')))
+    for _ in range(m)
+]
+
+# 1. points 정렬
+points.sort(key=lambda x:(x[0], x[1]))
+key_lst = [x[0] for x in points]
+# print(key_lst)
+# print(bisect_left(key_lst, 3))
+# print(bisect_right(key_lst, 6))
+
+for target in targets:
+    x1, y1 = target
+    x_idx = bisect_left(key_lst, x1+1)
+    if x_idx >= n:
+        print(*[-1, -1])
+        continue
+    if points[x_idx][0] >= x1:
+        print(f'{points[x_idx][0]} {points[x_idx][1]}')
+        continue
+    else:
+        print(*[-1, -1])
