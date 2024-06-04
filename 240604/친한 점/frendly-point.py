7dceb9242b25aa070f1ca2a3223e505fe63fd892
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-06-04 23:53:54 +0900

    Title: 친한 점, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240604/친한 점/frendly-point.py
+++ 240604/친한 점/frendly-point.py
@@ -21,12 +21,17 @@ key_lst = [x[0] for x in points]
 
 for target in targets:
     x1, y1 = target
-    x_idx = bisect_left(key_lst, x1+1)
-    if x_idx >= n:
-        print(*[-1, -1])
+    x0_idx = bisect_left(key_lst, x1)
+    if points[x0_idx][0] == x1:
+        print(f'{points[x0_idx][0]} {points[x0_idx][1]}')
         continue
+    x_idx = bisect_left(key_lst, x1+1)
     if points[x_idx][0] >= x1:
         print(f'{points[x_idx][0]} {points[x_idx][1]}')
+        # print(f'ddd: {x_idx}')
+        continue
+    if x_idx >= n:
+        print(*[-1, -1])
         continue
     else:
         print(*[-1, -1])
