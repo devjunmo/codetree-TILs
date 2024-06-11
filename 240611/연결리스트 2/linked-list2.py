3e2cf85652909ac871377f4027a881d9e06d2af6
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-06-11 19:32:41 +0900

    Title: 연결리스트 2, Time: 144ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240611/연결리스트 2/linked-list2.py
+++ 240611/연결리스트 2/linked-list2.py
@@ -14,13 +14,34 @@ for i in range(1,n+1):
 
 
 def pop_node(node):
-    pass
+    if node.prev is not None:
+        node.prev.next=node.next
+    if node.next is not None:
+        node.next.prev=node.prev
+    
+    node.prev=None
+    node.next=None
 
-def insert_node_front(i):
-    pass
 
-def insert_node_back(i):
-    pass
+def insert_node_front(node, i):
+    tgt_node=node_lst[i]
+    node.prev=tgt_node.prev
+    node.next=tgt_node
+
+    if tgt_node.prev is not None:
+        tgt_node.prev.next=node
+    tgt_node.prev=node
+    
+
+def insert_node_back(node, i):
+    tgt_node=node_lst[i]
+    node.prev=tgt_node
+    node.next=tgt_node.next
+
+    if tgt_node.next is not None:
+        tgt_node.next.prev=node
+    tgt_node.next=node
+
 
 for i in range(q):
     query = tuple(map(int, input().strip().split(' ')))
@@ -31,7 +52,25 @@ for i in range(q):
         pop_node(node)
     elif query[0] == 2:
         j = query[2]
+        insert_node_front(node_lst[j],i)
     elif query[0] == 3:
         j = query[2]
+        insert_node_back(node_lst[j],i)
     elif query[0] == 4:
-        pass
+        v1,v2=0,0
+        n1=node_lst[i].prev
+        n2=node_lst[i].next
+        if n1 is not None:
+            v1=n1
+        if n2 is not None:
+            v2=n2
+        print(f'{v1} {v2}')
+
+res=[]
+for i in range(1,n+1):
+    if node_lst[i].next is not None:
+        res.append(node_lst[i].next.node_numb)
+    else:
+        res.append(0)
+
+print(*res)
