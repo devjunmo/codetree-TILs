4e3b08eb5f30568f304efef4a25dc961ee9ea4e3
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-06-18 19:49:35 +0900

    Title: 겹치는 선분들, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240618/겹치는 선분들/overlapping-lines.py
+++ 240618/겹치는 선분들/overlapping-lines.py
@@ -20,6 +20,38 @@ points.sort()
 # (x1, +1, i)
 # (x2, -1, i)
 # 위 같은 형태로 튜플 적재
-
+segments=[]
+for i, (x1,x2) in enumerate(points):
+    segments.append((x1,1,i))
+    segments.append((x2,-1,i))
+segments.sort()
+trigg=False
+ans=0
+segset=set()
 # set에 1일때 i를 넣고 
-# 길이가 k잇
+# 길이가 k이상이면서 
+# 트리거 False면 
+# ans+=1 하고 트리거 True
+
+# -1일때 i를빼고 
+# 길이가 k미만이면 트리거 False
+open_x=0
+for x,v,i in segments:
+    if v==1:
+        segset.add(i)
+        #print(segset)
+        if len(segset)>=k and not trigg:
+            #ans+=1
+            trigg=True
+            open_x = x
+
+    else:
+        segset.remove(i)
+        #print(ans)
+        if len(segset) < k:
+            trigg=False
+            ans+=abs(open_x-x)
+            open_x=x
+
+
+print(ans)
