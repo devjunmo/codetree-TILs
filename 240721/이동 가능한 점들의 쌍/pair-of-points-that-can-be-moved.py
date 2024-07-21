fdeda60d3867245ab7162b5d779b5465fbf6c531
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-07-21 19:54:10 +0900

    Title: 이동 가능한 점들의 쌍, Time: 444ms, Memory: 27MB, Status: Passed - CodeTree

--- 240721/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
+++ 240721/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
@@ -2,39 +2,45 @@ import sys
 
 MAX_INT = sys.maxsize
 
-# 빨간점일때만 위치 갱신 (1~P)
-N, M, P, Q = tuple(map(int, input().strip().split(' ')))
+# 입력 받기
+N, M, P, Q = map(int, input().strip().split())
 
-dist = [
-    [MAX_INT] * (N+1) for _ in range(N+1)
-]
+# 그래프 초기화
+dist = [[MAX_INT] * (N + 1) for _ in range(N + 1)]
+red_points = set(range(1, P + 1))
 
-for m in range(M):
-    src, tgt, cost = tuple(map(int, input().strip().split(' ')))
+# 자기 자신으로 가는 거리는 0
+for i in range(1, N + 1):
+    dist[i][i] = 0
+
+# 간선 정보 입력받기
+for _ in range(M):
+    src, tgt, cost = map(int, input().strip().split())
     dist[src][tgt] = cost
 
-queries = []
+# 쿼리 정보 입력받기
+queries = [tuple(map(int, input().strip().split())) for _ in range(Q)]
 
-for q in range(Q):
-    queries.append(tuple(map(int, input().strip().split(' '))))
-
-for k in range(1, P+1):
-    for i in range(1, N+1):
-        for j in range(1, N+1):
-            if dist[i][k] == MAX_INT or dist[k][j] == MAX_INT:
-                continue
-            nxt_dst = dist[i][k] + dist[k][j]
-            if dist[i][j] > nxt_dst:
-                dist[i][j] = nxt_dst
+# 플로이드-워셜 알고리즘 수행
+for k in range(1, N + 1):
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            if dist[i][k] < MAX_INT and dist[k][j] < MAX_INT:
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
 cnt = 0
 cost_sum = 0
 
+# 쿼리 처리
 for s, t in queries:
-    v = dist[s][t]
-    if v != MAX_INT:
-        cnt+=1
-        cost_sum+=v
+    min_cost = MAX_INT
+    for r in red_points:
+        if dist[s][r] < MAX_INT and dist[r][t] < MAX_INT:
+            min_cost = min(min_cost, dist[s][r] + dist[r][t])
+    
+    if min_cost < MAX_INT:
+        cnt += 1
+        cost_sum += min_cost
 
 print(cnt)
 print(cost_sum)
