882eaee3fcd2065cd6531912781e01f42b20532e
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-07-21 18:51:15 +0900

    Title: 이동 가능한 점들의 쌍, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240721/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
@@ -0,0 +1,38 @@
+import sys
+
+MAX_INT = sys.maxsize
+
+# 빨간점일때만 위치 갱신 (1~P)
+N, M, P, Q = tuple(map(int, input().strip().split(' ')))
+
+dist = [
+    [MAX_INT] * (N+1) for _ in range(N+1)
+]
+
+for m in range(M):
+    src, tgt, cost = tuple(map(int, input().strip().split(' ')))
+    dist[src][tgt] = cost
+
+queries = []
+
+for q in range(Q):
+    queries.append(tuple(map(int, input().strip().split(' '))))
+
+for k in range(1, P+1):
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            nxt_dst = dist[i][k] + dist[k][j]
+            if dist[i][j] > nxt_dst:
+                dist[i][j] = nxt_dst
+
+cnt = 0
+cost_sum = 0
+
+for s, t in queries:
+    v = dist[s][t]
+    if v != MAX_INT:
+        cnt+=1
+        cost_sum+=v
+
+print(cnt)
+print(cost_sum)
