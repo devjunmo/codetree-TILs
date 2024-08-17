6bcd97d11c933c11969158620933acb5e22f772b
Junmo Yang <55372995+devjunmo@users.noreply.github.com>
2024-08-17 23:48:03 +0900

    Title: n개의 점 중 m개 고르기, Time: 249ms, Memory: 28MB, Status: Passed - CodeTree

--- 240817/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240817/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -16,7 +16,8 @@ for _ in range(n):
     points.append(tuple(map(int, input().strip().split(' '))))
 
 def get_dist(p1, p2):
-    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+    return (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2
+    # return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
 
 ans = sys.maxsize
 
@@ -35,4 +36,5 @@ for comb_idx in comb_idx_lst:
             max_y_idx = i2
     ans = min(ans, max_dist)
 
-print(int(ans**2))
+# print(int(ans**2))
+print(ans)
